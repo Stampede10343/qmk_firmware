ceb34476a2f2dd9007083f1dd6d144cc58ce207c
Cameron Cordes <cameronc.dev@gmail.com>
2019-11-30 00:13:01 -0500

    First draft on gherkin layout
    Still working through some weirdness with timings, ensuring I have all
    the necessary keys.

---
+++ keyboards/40percentclub/gherkin/keymaps/cameron/keymap.c
@@ -0,0 +1,33 @@
+#include QMK_KEYBOARD_H
+
+#define FN1_Q LT(1, KC_Q)
+#define LT3_SPC LT(3, KC_SPC)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [0] = LAYOUT_ortho_3x10(
+      FN1_Q,        KC_W,         KC_E,        KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+      LGUI_T(KC_A), KC_S,         KC_D,        KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SFTENT,
+      LCTL_T(KC_Z), LT(1, KC_X),  LT(2, KC_C), KC_V,   KC_BSPC, LT3_SPC, KC_B,    KC_N,    KC_M,    KC_GESC
+      ),
+
+  [1] = LAYOUT_ortho_3x10(
+      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_LEFT, KC_HOME, KC_UP,   KC_END,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT
+      ),
+
+  [2] = LAYOUT_ortho_3x10(
+      KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+      KC_F11,  KC_F12,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,
+      KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12
+      ),
+
+  [3] = LAYOUT_ortho_3x10(
+      KC_DEL,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC,  KC_MINS,
+      KC_TAB,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_SCOLON,
+      KC_BSLS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_LSFT,   KC_TRNS, KC_COMM, KC_DOT,  KC_QUOTE, KC_SLASH
+      )
+
+};
+
