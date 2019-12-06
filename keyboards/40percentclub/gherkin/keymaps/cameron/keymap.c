e40f05ed62ba0d5922e5e5b971db8e5d475a8d2b
Cameron Cordes <cameronc.dev@gmail.com>
2019-12-05 19:56:57 -0500

    More tweaks to the Gherkin layout
    Media controls and another way to access layer one.

--- keyboards/40percentclub/gherkin/keymaps/cameron/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/cameron/keymap.c
@@ -2,19 +2,20 @@
 
 #define FN1_Q LT(1, KC_Q)
 #define LT3_SPC LT(3, KC_SPC)
+#define LT1_ESC LT(1, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_ortho_3x10(
       FN1_Q,        KC_W,         KC_E,        KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
       LGUI_T(KC_A), KC_S,         KC_D,        KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SFTENT,
-      LCTL_T(KC_Z), LT(1, KC_X),  LT(2, KC_C), KC_V,   KC_BSPC, LT3_SPC, KC_B,    KC_N,    KC_M,    KC_GESC
+      LCTL_T(KC_Z), LT(1, KC_X),  LT(2, KC_C), KC_V,   KC_BSPC, LT3_SPC, KC_B,    KC_N,    KC_M,    LT1_ESC
       ),
 
   [1] = LAYOUT_ortho_3x10(
       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_LEFT, KC_HOME, KC_UP,   KC_END,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_TRNS, KC_TRNS, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
       ),
 
   [2] = LAYOUT_ortho_3x10(
@@ -25,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [3] = LAYOUT_ortho_3x10(
       KC_DEL,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC,  KC_MINS,
-      KC_TAB,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_SCOLON,
-      KC_BSLS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_LSFT,   KC_TRNS, KC_COMM, KC_DOT,  KC_QUOTE, KC_SLASH
+      KC_TAB,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_GRV, KC_VOLU, KC_MPLY, KC_VOLD,  KC_SCOLON,
+      KC_BSLS, KC_TRNS, KC_TRNS,  KC_LALT,  KC_LSFT,   KC_TRNS, KC_COMM, KC_DOT,  KC_QUOTE, KC_SLASH
       )
 
 };
