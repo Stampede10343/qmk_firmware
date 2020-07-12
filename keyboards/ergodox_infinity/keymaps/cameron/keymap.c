f94ce9de36d64ca041c0567a9d3c57beb7029abf
Cameron Cordes <cameronc.dev@gmail.com>
2020-07-12 09:17:32 -0400

    Fix key spacing

--- keyboards/ergodox_infinity/keymaps/cameron/keymap.c
+++ keyboards/ergodox_infinity/keymaps/cameron/keymap.c
@@ -125,11 +125,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    |     |     |     |
  *    +-----+-----+-----+
  */
-       KC_TRNS,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,
-                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-                         KC_VOLU, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_F6,   KC_F7,    KC_F8,      KC_F9,   KC_F10,  KC_F11,
+       KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS, KC_TRNS, KC_F12,
+                KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS, KC_TRNS, KC_MPLY,
+       KC_TRNS, KC_TRNS, KC_TRNS,  KC_MPRV,    KC_MNXT, KC_TRNS, KC_TRNS,
+                         KC_VOLUP, KC_VOLDOWN, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
